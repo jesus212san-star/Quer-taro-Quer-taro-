6c1c177192831ae38e8c9c51eef9743496a8f085
jesus212san-star <neko212jiromiya@gmail.com>
2026-04-19 15:36:01 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+// Mostrar solo una sección
+function mostrar(seccion){
+    let pantallas = document.querySelectorAll(".pantalla");
+
+    pantallas.forEach(p => {
+        p.style.display = "none";
+    });
+
+    document.getElementById(seccion).style.display = "block";
+}
+
+// Mensaje botón
 function mensaje(){
     alert("¡Gracias por visitar Querétaro!");
+}
+
+// Mostrar inicio por defecto
+window.onload = function(){
+    mostrar("inicio");
 }
